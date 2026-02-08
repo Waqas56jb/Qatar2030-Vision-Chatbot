dc682831de68c742f52eb4ea4219da7f6d48696f
Waqas56jb <hmza56jb@gmail.com>
2026-02-08 19:37:23 +0500

    sdf

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,10 +10,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-  },
-  base: '/',
 })
